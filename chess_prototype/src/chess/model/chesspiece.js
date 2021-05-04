7500cba5a049d5ba09bb541688b1bc589e5c5b72
max <maxmagi@hotmail.com>
2021-05-04 16:48:57 +0200

    mm

--- chess_prototype/src/chess/model/chesspiece.js
+++ chess_prototype/src/chess/model/chesspiece.js
@@ -1,8 +1,8 @@
 class ChessPiece {
     constructor(name, id, isAttacked, color) {
         this.name = name; // string
-        this.color = color // string: white | black
-        this.id = id // string
+        this.color = color; // string: white | black
+        this.id = id; // string
         this.isAttacked = isAttacked // boolean
     }
 
